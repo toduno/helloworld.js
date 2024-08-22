9eee321d6323b209b750b41866d343a6d23700f0
Todun <morountoduno@gmail.com>
2024-08-22 16:37:04 +0100

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,18 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(8080);
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end('Hello World!');
+// }).listen(8080);
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'Hello World' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
